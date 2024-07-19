78fbe474b7af01420ff63a9cf2fc32be418daf65
EonJo <sim04170@gmail.com>
2024-07-19 23:07:12 +0900

    05 - fix exception

--- 05/exception/exception_fix.py
+++ 05/exception/exception_fix.py
@@ -1,20 +1,29 @@
 def main():
     try:
         divisor = 0
-        x = 1 / divisor
-    except Exception as e:
-        print("Error", e)
-    
-    try:
-        my_dict = {'name': 'Alice'}
-        age = my_dict.get('age')
+        if divisor != 0:
+            x = 1 / divisor
+        else:
+            print("분모가 0입니다. 나눗셈을 수행할 수 없습니다.")
     except Exception as e:
         print("Error", e)
 
     try:
-        int('abc')  # ValueError
+        my_dict = {"name": "Alice"}
+        name = my_dict.get("name")
+        if name is None:
+            print("The key 'name' does not exist in the dictionary.")
+
+        print("Name : ", name)
+
     except Exception as e:
         print("Error", e)
+
+    try:
+        int(123)
+    except Exception as e:
+        print("Error", e)
+
 
 if __name__ == "__main__":
     main()
