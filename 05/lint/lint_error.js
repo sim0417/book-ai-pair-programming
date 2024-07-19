6089005d3851e00f8939962a6d2791554e0fcb39
EonJo <sim04170@gmail.com>
2024-07-19 22:45:25 +0900

    05 - fix lint error

--- 05/lint/lint_error.js
+++ 05/lint/lint_error.js
@@ -1,15 +1,13 @@
-/* eslint-disable no-console */
-var name = 'John Doe';
-var age = 30;
-var unusedVariable = 'This is not used anywhere';
+const name = "John Doe";
+const age = 30;
 
-if (name == 'John Doe') {
-  console.log('Welcome, John Doe!');
+if (name === "John Doe") {
+  console.log("Welcome, John Doe!");
 }
 
-console.log('This is a log message');
+console.log("This is a log message");
 
-var greeting = `Hello, ${name}! You are ${age} years old`;
+const greeting = `Hello, ${name}! You are ${age} years old`;
 
 if (age > 18) {
   console.log(greeting);
